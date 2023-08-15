7ee099807caf902057203ecc3c1f0e057b24d9c5
Thea <theacsullivan@gmail.com>
2023-08-15 18:07:36 +0100

    Setup react & added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+
+      <footer>
+        {" "}
+        Coded by Thea Sullivan and available on{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/TheacSully"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Github
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
