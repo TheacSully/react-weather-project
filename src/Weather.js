0d965dab3c2fca6732bcfcbdde014c58ca9f4c01
Thea <theacsullivan@gmail.com>
2023-08-18 15:46:30 +0100

    Added axios, added states & api calls

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  //const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  //const [temperature, setTemperature] = useState(null);
+  //const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: "Friday 17:00",
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "http://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a City"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul className="main">
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a City"
-              className="form-control"
-            />
+          <div className="col-6">
+            <div className="d-flex weather-temperature">
+              <img src="http://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units">°C</span>
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
-      <h1> New York</h1>
-      <ul className="main">
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div class="d-flex weather-temperature">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" />
-            <strong id="temperature">12°C</strong>
-          </div>
-        </div>
-        <div className="col-6">
-          <div className="details">
-            <ul>
-              <li>
-                Precipitation:<strong> 15% </strong>
-              </li>
-              <li>
-                Humidity:<strong> 15%</strong>
-              </li>
-              <li>
-                Wind Speed: <strong>15kmph </strong>
-              </li>
-            </ul>
+          <div className="col-6">
+            <div className="details">
+              <ul>
+                <li>
+                  Humidity:<strong> {weatherData.humidity}%</strong>
+                </li>
+                <li>
+                  Wind Speed:{" "}
+                  <strong>{Math.round(weatherData.wind)}kmph </strong>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b9ba0314a93083136d968577c718e31d";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
