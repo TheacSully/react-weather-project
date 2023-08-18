d43797b8f1529f269647ed25b414681404c7c806
Thea <theacsullivan@gmail.com>
2023-08-18 14:25:54 +0100

    STARTED AGAIN - setup components and added basic HTML

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import Weather from "./Weather";
-import Overview from "./Overview";
 
 export default function App() {
   return (
     <div className="App">
       <div className="Container">
-        <h1>Weather App</h1>
-        <Overview />
         <Weather />
       </div>
       <footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,40 @@
 import React from "react";
 import "./Weather.css";
-import Rainy from "./rainy.png";
 
-export default function Main() {
-  let weatherData = {
-    temperature: 14,
-    humidity: 80,
-    wind: 10,
-  };
-
+export default function Weather() {
   return (
-    <div className="row">
-      <div className="col-6">
-        <div className="d-flex weather-temperature">
-          <img src={Rainy} alt="" />
-          <strong>{weatherData.temperature}</strong>
-          <span className="units">
-            <p>°C|°F</p>
-          </span>
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a City"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
         </div>
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>
-            Humidity:
-            <span> {weatherData.humidity}</span>%
-          </li>
-          <li>
-            Wind:
-            <span> {weatherData.wind} </span>
-            kmph
-          </li>
-        </ul>
+      </form>
+      <h1> New York</h1>
+      <ul className="main">
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 15%</li>
+            <li>Wind Speed: 15kmph </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
